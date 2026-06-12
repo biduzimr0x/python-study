2d315c3d1923926579a796c9b3d0838195d4b3c4
Henrique <194309144+biduzimr0x@users.noreply.github.com>
2026-06-11 21:00:56 -0300

    Primeiros 3 exercícios foram resolvidos

---
+++ Exercícios/ex01.py
@@ -0,0 +1,4 @@
+'''Crie um script Python que leia o nome de uma pessoa e mostre uma mensagem de boas-vindas de acordo com o valor digitado'''
+print('====== DESAFIO 01 ======')
+nome = str(input('Qual é o seu nome? '))
+print('Olá {}! Prazer em te conhecer!'.format(nome))
--- Exercícios/ex02.py
+++ Exercícios/ex02.py
@@ -1,1 +1,6 @@
 """Crie um script Python que leia o dia, o mês e o ano de nascimento de uma pessoa e mostre uma mensagem com a data formatada"""
+dia = int(input('Digite o dia do seu nascimento: '))
+mês = int(input('Digite o mês do seu nascimento: '))
+ano = int(input('Digite o ano do seu nascimento: '))
+print('====== DESAFIO 02 ======')
+print('VOcê nasceu no dia {} de {} de {}. Correto?'.format(dia, mês, ano))
